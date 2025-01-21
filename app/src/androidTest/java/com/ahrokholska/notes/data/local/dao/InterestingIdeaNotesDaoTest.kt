fc2a5549fe229061d75b941f4690740bdd5767fc
Anastasiia <anastasiiahrokholska@gmail.com>
2025-01-21 11:01:57 +0200

    Added test for InterestingIdeaNotesDao delete function

--- app/src/androidTest/java/com/ahrokholska/notes/data/local/dao/InterestingIdeaNotesDaoTest.kt
+++ app/src/androidTest/java/com/ahrokholska/notes/data/local/dao/InterestingIdeaNotesDaoTest.kt
@@ -43,4 +43,31 @@ class InterestingIdeaNotesDaoTest {
         assertEquals(note.title, allNotes.first().title)
         assertEquals(note.body, allNotes.first().body)
     }
+
+    @Test
+    fun noteIsDeleted() = runTest {
+        val note = InterestingIdeaNoteEntity(title = "note1", body = "body1")
+        dao.insert(note)
+        var allNotes = dao.getAllInterestingIdeaNotes().first()
+        dao.delete(allNotes.first().id, {}, {})
+        allNotes = dao.getAllInterestingIdeaNotes().first()
+        assertEquals(0, allNotes.size)
+    }
+
+    @Test
+    fun onlyCorrectNoteIsDeleted() = runTest {
+        val note1 = InterestingIdeaNoteEntity(title = "note1", body = "body1")
+        val note2 = InterestingIdeaNoteEntity(title = "note2", body = "body2")
+        val note3 = InterestingIdeaNoteEntity(title = "note3", body = "body3")
+        dao.insert(note1)
+        dao.insert(note2)
+        dao.insert(note3)
+        var allNotes = dao.getAllInterestingIdeaNotes().first()
+        val note1Id =
+            allNotes.find { it.title == note1.title }?.id ?: throw Error("note was not inserted")
+        dao.delete(note1Id, {}, {})
+        allNotes = dao.getAllInterestingIdeaNotes().first()
+        assertEquals(2, allNotes.size)
+        assert(allNotes.all { it.title != note1.title })
+    }
 }
